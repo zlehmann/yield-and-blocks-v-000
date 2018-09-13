49250be9f7876db10f4bfedf7100694a662936e1
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-13 01:39:42 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,4 +8,8 @@ def hello_t(array)
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"])
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
